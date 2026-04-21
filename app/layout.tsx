5c68f2d3e2cdcf17a520592a12cc01fa3325d9df
imadkasse <kasseimad81@gmail.com>
2026-04-21 20:47:28 +0100

    feat: make the default theme as dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         className={`${unbounded.variable} ${questrial.variable} font-body antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange>
           {children}
